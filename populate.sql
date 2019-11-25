05ee28ce8ce56671e2533dbdc7a7d81d81ccfa85
tigofran <tigofran@gmail.com>
2019-11-25 18:56:30 +0000

    Populate.sql -> faltam tabelas

---
+++ populate.sql
@@ -0,0 +1,49 @@
+insert into local_publico(latitude,longitude,nome) values (38.697931, -9.206683, 'Mosteiro dos Jeronimos');
+insert into local_publico(latitude,longitude,nome) values (38.691617, -9.215977, 'Torre de Belem');
+insert into local_publico(latitude,longitude,nome) values (38.937000, -9.325951, 'Convento de Mafra');
+insert into local_publico(latitude,longitude,nome) values (41.145837, -8.614620, 'Torre dos Clerigos');
+insert into local_publico(latitude,longitude,nome) values (38.572632, -7.907273, 'Templo de Diana');
+insert into local_publico(latitude,longitude,nome) values (39.816082, -7.513033, 'Piscina Praia');
+insert into local_publico(latitude,longitude,nome) values (41.140085, -8.609449, 'Ponte Dom Luis');
+insert into local_publico(latitude,longitude,nome) values (38.693596, -9.205712, 'Padrao dos Descobrimentos');
+insert into local_publico(latitude,longitude,nome) values (38.725280, -9.150049, 'Praca do Marques');
+insert into local_publico(latitude,longitude,nome) values (40.101576, -7.313568, 'Mata da Rainha');
+insert into local_publico(latitude,longitude,nome) values (39.936529, -7.601457, 'Barbaido');
+insert into local_publico(latitude,longitude,nome) values (41.448172, -8.290265, 'Castelo de Guimaraes');
+insert into local_publico(latitude,longitude,nome) values (40.054725, -8.671053, 'Santo Isidro');
+insert into local_publico(latitude,longitude,nome) values (37.017578, -7.969428, 'Aeroporto de Faro');
+insert into local_publico(latitude,longitude,nome) values (37.073545, -8.115933, 'Casino Vilamoura');
+insert into local_publico(latitude,longitude,nome) values (36.997975, -8.948814, 'Fortaleza de Sagres');
+
+insert into item(descricao,localizacao,latitude,longitude) values ('Nao se diz porta minas', 'Santo Isidro',40.054725, -8.671053);
+insert into item(descricao,localizacao,latitude,longitude) values ('Nao se diz porta minas', 'Santo Isidro',40.054725, -8.671053);
+insert into item(descricao,localizacao,latitude,longitude) values ('A palavra nao tem acento', 'Templo de Diana', 38.572632, -7.907273);
+insert into item(descricao,localizacao,latitude,longitude) values ('Esta tudo mal escrito', 'Ponte Dom Luis', 41.140085, -8.609449);
+insert into item(descricao,localizacao,latitude,longitude) values ('Portugal nao tem dois O', 'Castelo de Guimaraes', 41.448172, -8.290265);
+insert into item(descricao,localizacao,latitude,longitude) values ('Steal esta mal escrito', 'Casino Vilamoura', 37.073545, -8.115933);
+insert into item(descricao,localizacao,latitude,longitude) values ('Steal esta mal escrito', 'Casino Vilamoura', 37.073545, -8.115933);
+
+insert into utilizador(email,pass) values ('jose@gmail.com', 'jose12345');
+insert into utilizador(email,pass) values ('manel@gmail.com', 'manel12345');
+insert into utilizador(email,pass) values ('miguel@gmail.com', 'miguel12345');
+insert into utilizador(email,pass) values ('joaquim@gmail.com', 'joaquim12345');
+insert into utilizador(email,pass) values ('alexandra@gmail.com', 'alexandra12345');
+insert into utilizador(email,pass) values ('palush@gmail.com', 'palush12345');
+insert into utilizador(email,pass) values ('dani@gmail.com', 'dani12345');
+insert into utilizador(email,pass) values ('user@hotmail.com', 'naotenhocriatividade');
+insert into utilizador(email,pass) values ('user2@webmail.com', 'continuosemcriatividade');
+insert into utilizador(email,pass) values ('user3@gmail.com', 'todaagenteusagmail');
+
+insert into utilizador_qualificado(email) values ('jose@gmail.com');
+insert into utilizador_qualificado(email) values ('manel@gmail.com');
+insert into utilizador_qualificado(email) values ('miguel@gmail.com');
+insert into utilizador_qualificado(email) values ('alexandra@gmail.com');
+
+insert into utilizador_regular(email) values ('joaquim@gmail.com');
+insert into utilizador_regular(email) values ('dani@gmail.com');
+insert into utilizador_regular(email) values ('palush@gmail.com');
+insert into utilizador_regular(email) values ('user@hotmail.com');
+insert into utilizador_regular(email) values ('user2@webmail.com');
+insert into utilizador_regular(email) values ('user3@gmail.com');
+
+
