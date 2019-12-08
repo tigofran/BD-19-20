0087129e9fb0365074fc8ef484cd06c535eb11c5
tigofran <tigofran@gmail.com>
2019-12-08 16:13:38 +0000

    Trigger initially deferred na RI-4

--- triggers.sql
+++ triggers.sql
@@ -1,3 +1,36 @@
+--RI_1
+
+drop trigger if exists zona_trigger on anomalia_traducao;
+create or replace function zona_trigger() returns trigger as $$
+begin
+    if (select id from anomalia natural join anomalia_traducao where (zona && zona2) is true) is not null
+    then
+        raise exception 'RI-1: A zona da anomalia_tradução nao se pode sobrepor a zona da anomalia correspondente';
+    end if;
+return new;
+end;
+$$
+language plpgsql;
+
+create trigger zona_trigger after insert or update on anomalia_traducao
+for each row execute procedure zona_trigger();
+
+--RI-4
+drop trigger if exists utilizador_trigger on utilizador;
+create or replace function utilizador_trigger() returns trigger as $$
+begin
+    if (select email from utilizador except (select email from utilizador_regular union select email from utilizador_qualificado)) is not null
+    then
+        raise exception 'RI-4: O email tem de estar em utilizador_qualificado ou utilizador_regular';
+    end if;
+return new;
+end;
+$$
+language plpgsql;
+
+create constraint trigger utilizador_trigger after insert or update on utilizador deferrable initially deferred
+for each row execute procedure utilizador_trigger();
+
 --RI_5
 
 drop trigger if exists utilizador_regular_trigger on utilizador_regular;
@@ -12,10 +45,10 @@ end;
 $$
 language plpgsql;
 
---RI-6
 create trigger utilizador_regular_trigger after insert or update on utilizador_regular
 for each row execute procedure utilizador_regular_trigger();
 
+--RI-6
 drop trigger if exists utilizador_qualificado_trigger on utilizador_qualificado;
 create or replace function utilizador_qualificado_trigger() returns trigger as $$
 begin
@@ -30,22 +63,3 @@ language plpgsql;
 
 create trigger utilizador_qualificado_trigger after insert or update on utilizador_qualificado
 for each row execute procedure utilizador_qualificado_trigger();
-
---RI-4
-create trigger utilizador_trigger after insert or update on utilizador
-for each row execute procedure utilizador_trigger();
-
-drop trigger if exists utilizador_trigger on utilizador;
-create or replace function utilizador_trigger() returns trigger as $$
-begin
-    if (select email from utilizador except (select email from utilizador_regular union select email from utilizador_qualificado)) is not null
-    then
-        raise exception 'RI-4: O email tem de estar em utilizador_qualificado ou utilizador_regular';
-    end if;
-return new;
-end;
-$$
-language plpgsql;
-
-create trigger utilizador_trigger after insert or update on utilizador
-for each row execute procedure utilizador_trigger();
