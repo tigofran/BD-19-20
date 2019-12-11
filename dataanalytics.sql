7ff7a40430730a32a0d3cb16311b2dfb9d31dc03
raquels29 <49168372+raquels29@users.noreply.github.com>
2019-12-11 20:16:00 +0000

    done

--- dataanalytics.sql
+++ dataanalytics.sql
@@ -1,20 +1,22 @@
-select tipo_anomalia, lingua, dia_da_anomalia, count(*) 
-from d_tempo natural join f_anomalia
-group by tipo_anomalia, lingua, dia_da_anomalia
+select tipo_anomalia, lingua, dia_da_semana, count(*) 
+from d_tempo natural join d_lingua natural join f_anomalia 
+group by tipo_anomalia, lingua, dia_da_semana
 
 union
 
 select tipo_anomalia, lingua, null, count(*) 
-from d_tempo natural join f_anomalia
+from d_tempo natural join d_lingua natural join f_anomalia 
 group by tipo_anomalia, lingua
 
 union
 
 select tipo_anomalia, null, null, count(*) 
-from d_tempo natural join f_anomalia
+from d_tempo natural join d_lingua natural join f_anomalia 
 group by tipo_anomalia
 
 union
 
 select null, null, null, count(*) 
-from d_tempo natural join f_anomalia;
+from d_tempo natural join d_lingua natural join f_anomalia
+
+order by tipo_anomalia, lingua, dia_da_semana;
